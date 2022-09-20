239bb90ed5239ab8f08776dbd3156bbb6e316f33
Sang kwon Yeum <kunshup2000@gmail.com>
2022-09-20 16:01:32 +0900

    feat: 라디오 버튼 구성

--- src/components/molecules/FormInput/RadioInput/RadioInput.style.ts
+++ src/components/molecules/FormInput/RadioInput/RadioInput.style.ts
@@ -1,3 +1,5 @@
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import { TextField } from 'components';
 import styled from '@emotion/styled';
 
 export const RadioInputContainer = styled.div`
@@ -5,4 +7,32 @@ export const RadioInputContainer = styled.div`
   height: fit-content;
 
   padding-top: 16px;
+`;
+
+export const RadioSelectionContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: start;
+  align-items: center;
+
+  box-sizing: border-box;
+`;
+
+export const RadioIcon = styled(CircleOutlinedIcon)`
+  color: #c4c4c4;
+`;
+
+export const RadioTextInput = styled(TextField)`
+  width: 80%;
+  margin-left: 8px;
+  & .MuiInput-root::before {
+    border: transparent;
+  }
+  & .MuiInput-root::after {
+    border-bottom: 2px solid #4c2b87;
+  }
+  & .MuiInput-root:hover::before {
+    border-bottom: 2px solid #dadce0;
+  }
 `;
--- src/components/molecules/FormInput/RadioInput/RadioInput.tsx
+++ src/components/molecules/FormInput/RadioInput/RadioInput.tsx
@@ -1,9 +1,119 @@
+import { useState } from 'react';
+import { nanoid } from '@reduxjs/toolkit';
+import ImageIcon from '@mui/icons-material/ImageOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from 'components';
 import * as S from './RadioInput.style';
 
+interface IRadioSelectionProps {
+  option: {
+    idx: number;
+    key: string;
+    value: string | number;
+    isMouseOver: boolean;
+    isFocused: boolean;
+  };
+  optionLength: number;
+  onMouseLeave: () => void;
+  onMouseOver: () => void;
+  onBlur: () => void;
+  onClick: () => void;
+}
+
+function RadioSelection({
+  option,
+  optionLength,
+  onMouseLeave,
+  onMouseOver,
+  onBlur,
+  onClick,
+}: IRadioSelectionProps) {
+  const { value, isMouseOver, isFocused } = option;
+
+  return (
+    <S.RadioSelectionContainer
+      onMouseOver={onMouseOver}
+      onMouseLeave={onMouseLeave}>
+      <S.RadioIcon />
+      <S.RadioTextInput
+        id='standard-basic'
+        variant='standard'
+        value={value}
+        onClick={onClick}
+        onBlur={onBlur}
+      />
+      {(isMouseOver || isFocused) && (
+        <IconButton style={{ marginLeft: '4px' }}>
+          <ImageIcon />
+        </IconButton>
+      )}
+      {optionLength > 1 && (
+        <IconButton style={{ marginLeft: '4px' }}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    </S.RadioSelectionContainer>
+  );
+}
+
 export function RadioInput() {
+  const [options, setOptions] = useState([
+    {
+      idx: 1,
+      key: nanoid(),
+      value: '옵션 1',
+      isMouseOver: false,
+      isFocused: false,
+    },
+  ]);
+
+  const onMouseOver = ({ key }: { key: string }) => {
+    setOptions(
+      options.map(option =>
+        option.key === key ? { ...option, isMouseOver: true } : { ...option },
+      ),
+    );
+  };
+
+  const onMouseLeave = ({ key }: { key: string }) => {
+    setOptions(
+      options.map(option =>
+        option.key === key ? { ...option, isMouseOver: false } : { ...option },
+      ),
+    );
+  };
+
+  const onClick = ({ key }: { key: string }) => {
+    setOptions(
+      options.map(option =>
+        option.key === key
+          ? { ...option, isFocused: true }
+          : { ...option, isFocused: false },
+      ),
+    );
+  };
+
+  const onBlur = ({ key }: { key: string }) => {
+    setOptions(
+      options.map(option =>
+        option.key === key ? { ...option, isFocused: false } : { ...option },
+      ),
+    );
+  };
+
   return (
     <S.RadioInputContainer>
-      <h1>RadioInput</h1>
+      {options.map(option => (
+        <RadioSelection
+          key={option.key}
+          option={option}
+          optionLength={options.length}
+          onMouseLeave={() => onMouseLeave({ key: option.key })}
+          onMouseOver={() => onMouseOver({ key: option.key })}
+          onBlur={() => onBlur({ key: option.key })}
+          onClick={() => onClick({ key: option.key })}
+        />
+      ))}
     </S.RadioInputContainer>
   );
 }
