d03b5d77976004e6e962cce13fcfd4d7d55642a3
Sang kwon Yeum <kunshup2000@gmail.com>
2022-09-21 13:36:31 +0900

    feat: 헬멧 적용 및 레이아웃 적용

--- src/Page/Result.tsx
+++ src/Page/Result.tsx
@@ -1,7 +1,18 @@
+import { Helmet } from 'react-helmet-async';
+import { useAppSelector } from 'hooks';
+import { Layout } from 'components';
+
 export function Result() {
+  const { items } = useAppSelector(state => state.form);
+
+  const [titleInfo] = items.filter(item => item.type === 'title');
+
+  if (!items || !titleInfo || items.length <= 1) return null;
+
   return (
-    <div>
-      <h1>Result</h1>
-    </div>
+    <>
+      <Helmet title={titleInfo.title} />
+      <Layout isMain={false}>Result</Layout>
+    </>
   );
 }
