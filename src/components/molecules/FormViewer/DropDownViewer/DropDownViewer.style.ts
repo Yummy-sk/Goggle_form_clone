2166353dc40b2702b6383dc83476a93a5087eb59
Sang kwon Yeum <kunshup2000@gmail.com>
2022-09-21 12:43:48 +0900

    feat: 드롭 다운 컴포넌트 뷰어 구성

--- src/components/molecules/FormViewer/DropDownViewer/DropDownViewer.style.ts
+++ src/components/molecules/FormViewer/DropDownViewer/DropDownViewer.style.ts
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
@@ -5,4 +6,25 @@ export const Container = styled.div`
   height: fit-content;
 
   margin-top: 16px;
+`;
+
+export const ViewerContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+`;
+
+export const ViewerContentWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: start;
+  align-items: center;
+
+  padding: 8px 0;
+`;
+
+export const ViewerContent = styled(Typography)`
+  &:not(:last-child) {
+    margin-right: 8px;
+  }
 `;
